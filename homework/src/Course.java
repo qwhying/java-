92869607e9438c1dbb83c10b708e9f54c8e43a58
qwhying <woshiqiwanga@163.com>
2018-10-12 17:39:49 +0800

    java_homework 10.9

---
+++ homework/src/Course.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Qwhying on 2018/10/12
+ */
+public class Course {
+    private String  courseName;
+    private int size=100;
+    private String[] students=new String[size];
+    private int numberOfStudents;
+    public Course(String courseName)
+    {
+        this.courseName=courseName;
+    }
+    public void addStudent(String student)
+    {
+        if(numberOfStudents>size){
+            String[] temp=new String[size*2];
+            students=temp;
+            size*=2;
+        }
+        students[numberOfStudents]=student;
+        numberOfStudents++;
+    }
+    public String[] getStudents()
+    {
+        return students;
+    }
+    public int getNumberOfStudents()
+    {
+        return numberOfStudents;
+    }
+    public String getCourseName()
+    {
+        return courseName;
+    }
+    public void dropStudent(String student)
+    {
+        for(int i=0;i<numberOfStudents;i++)
+        {
+            if(students[i].equals(student)) {
+                for (int j = i + 1; j < numberOfStudents; j++) {
+                    students[j - 1] = students[j];
+                }
+                students[numberOfStudents - 1] = "";
+                numberOfStudents--;
+            }
+        }
+
+    }
+    public void clear(){
+
+    }
+}
