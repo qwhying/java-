96fd27904f414a2ee719b606c620e7c00fffe172
qwhying <woshiqiwanga@163.com>
2018-10-31 12:57:56 +0800

    javacrawler

--- ATM/src/com/company/ATM.java
+++ ATM/src/com/company/ATM.java
@@ -1,8 +1,11 @@
 package com.company;
 
 /**
- * Created by Qwhying on 2018/10/16
- */
+ * Created by Qwhying on 2018/10/16*/
+import java.util.*;
+
+import static java.lang.System.exit;
+
 public class ATM{
     private int id=0;//用户id
     private double balance=0;//余额
@@ -17,6 +20,10 @@ public class ATM{
     }
     public void withdraw(double amount)
     {
+        if(balance<amount) {
+            System.out.println("You don't have enough money on your account.");
+        }
+        else
         balance-=amount;
     }
     public void deposit(double amount)
